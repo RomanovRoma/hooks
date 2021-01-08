bc106c6815a7fec97111f32473c45bc9b3fa5dca
RomanovRoma <romanian.reget@gmail.com>
2021-01-08 11:12:49 +0300

    feat: add useContext

--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 const App = () => {
-  return (
-    <div>
-      <HookSwitcher />
-    </div>
-  )
+
 }
 
-const HookSwitcher = () => {
-
-  const [ color, setColor ] = useState('grey')
-  const [ fontSize, setFontSize ] = useState(14)
-
-  return (
-    <div style={{ padding: '10px',
-      backgroundColor: color,
-      fontSize: `${fontSize}px` }}>
-        Hello React Hooks!
-      <button
-        onClick={() => setColor('grey')}>
-        Dark
-      </button>
-      <button
-        onClick={() => setColor('white')}>
-        Light
-      </button>
-      <button
-        onClick={() => setFontSize((s) => s + 2)}>
-        +
-      </button>
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
