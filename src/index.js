dce37177d9f7c27231815afbf516d7a2c47c6fb6
RomanovRoma <romanian.reget@gmail.com>
2021-01-10 12:55:45 +0300

    feat: add useEffect() - exercises

--- src/index.js
+++ src/index.js
@@ -1,56 +1,53 @@
-import React, { Component, useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { isElement } from "react-dom/test-utils";
 
 const App = () => {
- const [value, setValue] = useState(0)
- const [visible, setVisible] = useState(true)
+  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(true);
 
- if (visible) {
-   return (
-     <div>
-       <button
-        onClick={() => setValue((v) => v + 1)}>+</button>
-      <button
-        onClick={() => setVisible(false)}>hide</button>
-      <ClassCounter value={value} />
-      <HookCounter value={value} />
-     </div>
-   )
- } else {
-   return (
-     <button onClick={() => setVisible(true)}>show</button>
-   )
- }
-}
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue((v) => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <HookCounter value={value} />
+        <Notification />
+      </div>
+    );
+  } else {
+    return <button onClick={() => setVisible(true)}>show</button>;
+  }
+};
 
 const HookCounter = ({ value }) => {
 
   useEffect(() => {
-    console.log('useEffect()')
-    return () => console.log('clear')
-  })
+    console.log('mount')
+    return () => console.log('unmount')
+  }, [])
 
-  return <p> {value} </p>
+  useEffect(() => console.log('update'))
+
+  return <p> {value} </p>;
+};
+
+const Notification = () => {
+
+  const [visible, setVisible] = useState(true)
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => setVisible(false), 2500)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <div>
+      { visible && <p>Hello useEffect</p>}
+    </div>
+  )
 }
 
-class ClassCounter extends Component {
 
-  componentDidMount() {
-    console.log('class: mount')
-  }
 
-  componentDidUpdate() {
-    console.log('class: update')
-  }
-
-  componentDidCatch() {
-    console.log('class: unmount')
-  }
-
-  render() {
-    return <p>{this.props.value}</p>
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
